a16d2a7039531ac3fe742af57795083b9b7fc552
Sicelo Gumede <12smgumede@gmail.com>
2023-03-14 05:00:21 +0200

    Now working on the nav bar responsivness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,40 @@
-import { React } from "react";
+import { React, Suspense, useState } from "react";
 import CallToAction from "./SvgComponent";
-import { OrbitControls, Stage, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls, Bounds, ContactShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Octopus from "./Octopus";
-import { Suspense } from "react";
-import { Bounds, useBounds, ContactShadows, useGLTF } from "@react-three/drei";
+import { FcMenu, FcHome, FcFolder, FcCollaboration } from "react-icons/fc";
 
 import website_logo from "/src/assets/website_logo.png";
 
-const Navbar = () => {
+const Hero = () => {
+  const [touchedHamburger, setHamburgerState] = useState(false);
 
-  <link href="dist/hamburgers.css" rel="stylesheet"></link>
-  
+  function changeHamburgerState() {
+    setHamburgerState(!touchedHamburger);
+  }
+
   return (
-    <div className="h-screen curve bg-[url('/src/assets/background.jpg')] bg-cover snap-center">
+    <div className={`h-screen bg-cover snap-center ${touchedHamburger ? 'bg-black/30' : "bg-[url('/src/assets/background.jpg')]"}`}>
+      {/* Hamburger NavBar */}
+
+      <div className={`fixed ${touchedHamburger ? 'bg-rose-100 w-3/5 h-screen ease-in duration-1000' : ' left-[-100%] ease-in duration-1000'}`}>
+        <div className="flex justify-center pt-5">
+          <img src={website_logo} width={180}></img>
+        </div>
+
+        <div>
+          <ul className={`absolute left-[10%] mt-10`}>
+            <li className="pb-3 flex gap-3"><span><FcHome/></span>Home</li>
+            <li className="pb-3 flex gap-3"><span><FcFolder/></span>Projects</li>
+            <li className="pb-3 flex gap-3"><span><FcCollaboration/></span>Connect</li>
+          </ul>
+        </div>
+      </div>
+
       {/* NavBar */}
       <div className="p-5 h-[10%]">
-        <div className="absolute left-5 top-2">
+        <div className={`absolute left-5 top-2 ${touchedHamburger ? 'hidden' : 'block'}`}>
           <img src={website_logo} width={180}></img>
         </div>
         <div className="absolute right-5 hidden md:block text-white">
@@ -26,13 +44,19 @@ const Navbar = () => {
             <li>Connect</li>
           </ul>
         </div>
+
+        {/* Hamburger */}
+        <div className="md:hidden absolute right-5 top-4">
+          <span>
+            <FcMenu size={30} onClick={changeHamburgerState} />
+          </span>
+        </div>
       </div>
 
       {/* Hero */}
-      <div className="grid-rows-2 md:grid-cols-2 flex md:flex-row flex-col justify-center items-center md:h-[90%]">
-
+      <div className={`grid-rows-2 md:grid-cols-2 flex md:flex-row flex-col justify-center items-center md:h-[90%] ${touchedHamburger ? 'hidden' : 'block'}`}>
         {/* Text Hero */}
-        <div className="w-1/2 text-center md:mb-32">
+        <div className="w-1/2 text-center">
           <h1 className="whitespace-nowrap pb-5 text-5xl text-indigo-600 font-bold">
             Creative
           </h1>
@@ -44,7 +68,9 @@ const Navbar = () => {
           </h1>
           <p>Do those words sound like your kind of developer?</p>
           <p>Then scroll down to learn more about me.</p>
-          <div className="ml-[45%]"><CallToAction /></div>
+          <div className="md:ml-[45%] ml-[40%]">
+            <CallToAction />
+          </div>
         </div>
 
         {/* Model Hero */}
@@ -93,4 +119,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Hero;
